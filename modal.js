e989aac3e45844e7fc354395b0becde5307d98cf
Henderson <hendersonsousa5@gmail.com>
2024-10-15 17:19:43 -0300

    first commit

---
+++ modal.js
@@ -0,0 +1,18 @@
+export const Modal = {
+
+    Wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+  
+  
+    open() {
+       Modal.Wrapper.classList.add('open')
+    },
+    close() {
+       Modal.Wrapper.classList.remove('open')
+    }
+  }
+
+  Modal.buttonClose.onclick = () => {
+    Modal.close()
+  }
